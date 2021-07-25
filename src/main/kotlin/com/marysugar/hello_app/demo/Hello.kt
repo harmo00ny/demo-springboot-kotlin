a02cbb527f0bc1fbd87351213ba4fc7c8e3fc4ac
harmo00ny <iseekana@gmail.com>
2021-07-26 00:20:09 +0900

    init

---
+++ src/main/kotlin/com/marysugar/hello_app/demo/Hello.kt
@@ -0,0 +1,12 @@
+package com.marysugar.hello_app.demo
+
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RestController
+
+@RestController
+class Hello {
+    @RequestMapping("/")
+    fun index() : String {
+        return "Hello World!"
+    }
+}
